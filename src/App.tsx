b9c82644b82467575697e07b23431b7c7b6c642b
jaeochoii <jaeochoiii0716@inha.edu>
2024-01-14 23:42:01 +0900

    First Commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import Layout from "./layout/Layout";
+
+const App = () => {
+  return (
+    <Layout>
+      <h1>내용을 작성해주세요.</h1>
+    </Layout>
+  );
+};
+
+export default App;
---
+++ src/layout/Header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <>
+      <header>헤더 컴포넌트 입니다.</header>
+    </>
+  );
+};
+
+export default Header;
